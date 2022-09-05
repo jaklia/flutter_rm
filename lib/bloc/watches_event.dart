79471befd9e28dc3db670faf4981a5ea386968a6
Jakli Aida <aidajakli@gmail.com>
2022-09-06 01:47:15 +0200

    bloc, repo, provider + mock data

---
+++ lib/bloc/watches_event.dart
@@ -0,0 +1,7 @@
+abstract class WatchListEvent {
+  const WatchListEvent();
+}
+
+class WatchListRequested extends WatchListEvent {
+  const WatchListRequested();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rm/bloc/watches_bloc.dart';
+import 'package:flutter_rm/bloc/watches_event.dart';
+import 'package:flutter_rm/repository/watches_repository.dart';
 import 'package:flutter_rm/pages/home_page.dart';
 
 void main() {
@@ -9,13 +13,25 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        iconTheme: IconThemeData(color: Colors.indigo[900]),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => WatchListBloc(
+            watchesRepository:
+                WatchesRepository(), // TODO:   this is not good, need other provider for the repo
+          )..add(
+              const WatchListRequested(),
+            ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.indigo[900]),
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
